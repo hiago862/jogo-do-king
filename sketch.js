5c36bb2457b54f73cec9ad47aa820239d9283e32
hiago862 <hiago.lisboa@escola.pr.gov.br>
2024-09-12 11:05:07 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,21 @@
+https://github.com/hiago862setup () 
+  createCanvas(400, 400);
+
+
+function draw() {
+  background(220);
+}function draw() {
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER)
+  
+//  if(mouseX < 50){
+//    let palavra = "C";
+//    text(palavra, 200, 200);
+//  } else {
+//    let palavra = "Caminhante";
+//    text(palavra, 200, 200);
+//  }
+}
+let quantidade = map(mouseX, 0, width, 1, palavra.length);
